8de415311888110bc1b4d59a257aef31f4025fc9
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-04-27 14:56:29 -0500

    add: gameboard + Player construct

--- script.js
+++ script.js
@@ -21,16 +21,4 @@ const player1 = new Player('player1', 'O');
 const player2 = new Player('player2', 'X');
 
 const Game = (() => {
-//   const score = 0;
-  const checkWin = (player) => {
-    player.choices.includes([1, 2, 3] || [4, 5, 6] || [7, 8, 9]
-        || [1, 4, 7] || [2, 5, 8] || [3, 6, 9]
-        || [1, 5, 9] || [3, 6, 9]);
-  };
-
-  if (checkWin(player1) || checkWin(player2)) {
-    console.log('WIN');
-  } else if (gameboard === []) {
-    console.log('TIE');
-  }
 })();
