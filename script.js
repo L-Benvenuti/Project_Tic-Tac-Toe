9671c7cc1fb0cedc9c9aaa2864c4c9046cdafb0b
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-05-01 15:40:31 -0500

    add/fix: Game function

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-function Player(name, mark) {
-  this.name = name;
-  this.mark = mark;
-  //   const choices = [];
-  //   // function addMark(e) {
-  //   //   const display = document.createElement('div');
-  //   //   display.classList.add('circle');
-  //   //   e.target.append(display);
-  //   // }
-  return {
-    name, mark, // , choices, addMark,
-  };
-}
+const Game = (() => {
+  let turn = 'circle';
+  function addMark(e) {
+    const display = document.createElement('div');
+    display.classList.add(turn);
+    e.target.append(display);
 
-function addMark(e) {
-  const display = document.createElement('div');
-  display.classList.add('cross');
-  e.target.append(display);
-}
-const player1 = new Player('player1', 'O');
-const player2 = new Player('player2', 'X');
+    turn = turn === 'circle' ? 'cross' : 'circle';
+    e.target.removeEventListener('click', addMark);
+  }
+
+  return { addMark };
+})();
 
 const GameBoard = (() => {
   const board = document.getElementById('gameboard');
@@ -27,11 +19,16 @@ const GameBoard = (() => {
     const square = document.createElement('div');
     square.classList.add('square');
     square.id = index;
-    square.addEventListener('click', addMark);
+    square.addEventListener('click', Game.addMark);
     board.appendChild(square);
   });
 })();
 
-// const Game = {
+function Player(name, mark) {
+  this.name = name;
+  this.mark = mark;
+  return { name, mark };
+}
 
-// }
+const player1 = new Player('player1', 'O');
+const player2 = new Player('player2', 'X');
