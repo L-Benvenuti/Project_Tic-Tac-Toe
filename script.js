7a244c44eb5da1e614019c06bfa07c99b7a933fd
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-05-01 16:22:05 -0500

    add: checkScore function - currently game does not require creation of players/does not allow for mark choice nor has bot option

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 const Game = (() => {
   let turn = 'circle';
+
+  function checkScore() {
+    const allSquares = document.querySelectorAll('.square');
+    const winningOptions = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6],
+    ];
+
+    winningOptions.forEach((array) => {
+      const circleWins = array.every((cell) => allSquares[cell].firstChild?.classList.contains('circle'));
+      const crossWins = array.every((cell) => allSquares[cell].firstChild?.classList.contains('cross'));
+
+      if (circleWins) {
+        console.log('circle won!');
+        allSquares.forEach((square) => square.replaceWith(square.cloneNode(true)));
+      } else if (crossWins) {
+        console.log('cross won!');
+        allSquares.forEach((square) => square.replaceWith(square.cloneNode(true)));
+      }
+    });
+  }
+
   function addMark(e) {
     const display = document.createElement('div');
     display.classList.add(turn);
@@ -7,6 +30,7 @@ const Game = (() => {
 
     turn = turn === 'circle' ? 'cross' : 'circle';
     e.target.removeEventListener('click', addMark);
+    checkScore();
   }
 
   return { addMark };
@@ -22,13 +46,15 @@ const GameBoard = (() => {
     square.addEventListener('click', Game.addMark);
     board.appendChild(square);
   });
+
+  return { cells };
 })();
 
-function Player(name, mark) {
-  this.name = name;
-  this.mark = mark;
-  return { name, mark };
-}
+// function Player(name, mark) {
+//   this.name = name;
+//   this.mark = mark;
+//   return { name, mark };
+// }
 
-const player1 = new Player('player1', 'O');
-const player2 = new Player('player2', 'X');
+// const player1 = new Player('player1', 'O');
+// const player2 = new Player('player2', 'X');
