45c0c469dc34366cc7d6fb410185fa83fd2238e3
HBL <115602697+L-Benvenuti@users.noreply.github.com>
2023-05-01 15:06:24 -0500

    fix: gameboard + add: addMark function and styling for squares + circle mark

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-const GameBoard = (() => {
-  const board = document.getElementById('gameboard');
-  for (let i = 1; i < 10; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.textContent = i;
-    board.appendChild(square);
-  }
-});
-
-GameBoard();
-
-const Player = function (name, mark) {
+function Player(name, mark) {
   this.name = name;
   this.mark = mark;
-  const choices = [];
-  return { name, mark, choices };
-};
+  //   const choices = [];
+  //   // function addMark(e) {
+  //   //   const display = document.createElement('div');
+  //   //   display.classList.add('circle');
+  //   //   e.target.append(display);
+  //   // }
+  return {
+    name, mark, // , choices, addMark,
+  };
+}
 
+function addMark(e) {
+  const display = document.createElement('div');
+  display.classList.add('circle');
+  e.target.append(display);
+}
 const player1 = new Player('player1', 'O');
 const player2 = new Player('player2', 'X');
 
-const Game = (() => {
+const GameBoard = (() => {
+  const board = document.getElementById('gameboard');
+  const cells = ['', '', '', '', '', '', '', '', ''];
+  cells.forEach((_cell, index) => {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.id = index;
+    square.addEventListener('click', addMark);
+    board.appendChild(square);
+  });
 })();
+
+// const Game = {
+
+// }
